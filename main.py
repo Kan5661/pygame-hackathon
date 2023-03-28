32cd2a592ce0e8e5092b054c489f7242183d61e2
Kan5661 <linkan0718@gmail.com>
2023-03-27 21:47:13 -0400

    draw player

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import sys
 
 py.init()
 screen = py.display.set_mode((800, 500))
+clock = py.time.Clock()
+
+def draw_player():
+    player_rect = py.image.load('./assets/player_assets/standing.png')
+    screen.blit(player_rect, (0, 0))
+
 
 RUN = True
 
@@ -11,3 +17,7 @@ while RUN:
     for event in py.event.get():
         if event.type == py.QUIT:
             py.quit(), sys.exit()
+    draw_player()
+    
+    py.display.update()
+    clock.tick(60)
